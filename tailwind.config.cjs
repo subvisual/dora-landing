90765d8a302ac1f3c0fe0fdae4bded882807d133
Paula Carneiro <paulacarneiro215@gmail.com>
2023-02-24 16:38:36 +0000

    define global styles and favicon

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,18 @@
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        black: "#0F1314",
+        yellow: "#FED766",
+        "light-gray": "#DAD9DB",
+        white: "#F4F7F5",
+        gray: "#575A5E",
+      },
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
