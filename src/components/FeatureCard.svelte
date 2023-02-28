25b05ff0f5263693067d51ff52cecce235064da7
Paula Carneiro <paulacarneiro215@gmail.com>
2023-02-28 16:16:31 +0000

    add features

---
+++ src/components/FeatureCard.svelte
@@ -0,0 +1,5 @@
+<div
+  class="w-full h-full flex justify-center items-center py-5 px-6 rounded-xl font-bold text-sm leading-6 bg-yellow text-black"
+>
+  <slot />
+</div>
---
+++ src/components/FeaturesGrid.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  import FeatureCard from "./FeatureCard.svelte";
+  import features from "../data/features";
+</script>
+
+<div class="grid md:grid-cols-2 w-11/12 sm:w-4/5 md:w-full mx-auto gap-x-16 gap-y-8 md:gap-y-10">
+  {#each features as feature, idx}
+    <FeatureCard>{feature}</FeatureCard>
+  {/each}
+</div>
