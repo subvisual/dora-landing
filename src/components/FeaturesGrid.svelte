c291071a5919a8effc5947fc70997641ff8bd803
Paula Carneiro <paulacarneiro215@gmail.com>
2023-02-28 17:23:37 +0000

    hide some features on mobile version

--- src/components/FeaturesGrid.svelte
+++ src/components/FeaturesGrid.svelte
@@ -1,10 +1,31 @@
 <script lang="ts">
+  /* eslint-disable svelte/no-at-html-tags */
   import FeatureCard from "./FeatureCard.svelte";
   import features from "../data/features";
+
+  let showAll = false;
+
+  function toggleShow() {
+    showAll = !showAll;
+  }
 </script>
 
-<div class="grid md:grid-cols-2 w-11/12 sm:w-4/5 md:w-full mx-auto gap-x-16 gap-y-8 md:gap-y-10">
-  {#each features as feature, idx}
-    <FeatureCard>{feature}</FeatureCard>
-  {/each}
+<div class="w-11/12 sm:w-4/5 md:w-full mx-auto">
+  <div class="hidden md:grid md:grid-cols-2 gap-x-16 gap-y-10">
+    {#each features as feature}
+      <FeatureCard>{feature}</FeatureCard>
+    {/each}
+  </div>
+  <div class="flex flex-col gap-8 md:hidden">
+    {#each features as feature, idx}
+      {#if idx < 3 || showAll}
+        <FeatureCard>{feature}</FeatureCard>
+      {/if}
+    {/each}
+    <button
+      class="w-fit mx-auto p-2"
+      on:click={toggleShow}
+      type="button">{@html showAll ? "&minus; Show less" : "&plus; Show more"}</button
+    >
+  </div>
 </div>
