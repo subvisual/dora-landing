e238b4c374fd9147658f6fa94bfeeeda90618e5b
Paula Carneiro <paulacarneiro215@gmail.com>
2023-03-01 16:44:59 +0000

    Add layout elements (#2)
    * define global styles and favicon
    
    * add header
    
    * add hero
    
    * add footer

---
+++ src/components/Button.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+  export let href: string;
+  export let header = false;
+</script>
+
+<a
+  {...$$restProps}
+  {href}
+  class:header
+>
+  <slot />
+</a>
+
+<style lang="postcss">
+  a {
+    @apply flex items-center whitespace-nowrap rounded-2xl bg-yellow py-3 px-6 tracking-tight text-black outline-black;
+  }
+
+  a:hover {
+    @apply brightness-105;
+  }
+
+  a.header {
+    @apply bg-transparent border border-yellow text-yellow py-2 px-5 sm:bg-yellow sm:text-black sm:border-none sm:py-3 sm:px-6;
+  }
+</style>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,28 @@
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {},
+    fontSize: {
+      sm: "1rem",
+      base: "1.125rem",
+      lg: "1.25rem",
+      xl: "1.5rem",
+      "2xl": "2rem",
+      "3xl": "3.75rem",
+      "4xl": "5.625rem",
+    },
+    extend: {
+      colors: {
+        black: "#0F1314",
+        yellow: "#FED766",
+        "light-gray": "#DAD9DB",
+        white: "#F4F7F5",
+        gray: "#575A5E",
+      },
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+        studio: ["Studio Feixen Sans", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
